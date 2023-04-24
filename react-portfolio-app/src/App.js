59da3d298caaa34c550b954c8f64ebdddf6ef9f6
Jags <jon.kmjt@gmail.com>
2023-04-24 13:30:07 -0400

    Commit before switching back to conditional pages

--- react-portfolio-app/src/App.js
+++ react-portfolio-app/src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import NavTabs from './components/NavTabs';
+import Header from './components/Header';
 import About from './components/pages/About';
 import Portfolio from './components/pages/Portfolio';
 import Contact from './components/pages/Contact';
 import Resume from './components/pages/Resume';
+import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div>
-			<NavTabs />
+			<Header />
 			<Routes>
 				<Route path="/about" element={<About />} />
 				<Route path="/portfolio" element={<Portfolio />} />
 				<Route path="/contact" element={<Contact />} />
 				<Route path="/resume" element={<Resume />} />
 			</Routes>
+			<Footer />
 		</div>
 	);
 }
